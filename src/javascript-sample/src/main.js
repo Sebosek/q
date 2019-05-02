50235c5fc246b625bf655a3b940053aae590f02d
Sebastian Bušek <sebastian.busek@konicaminolta.cz>
2019-05-02 19:01:51 +0200

    feat(): sample

--- src/javascript-sample/src/main.js
+++ src/javascript-sample/src/main.js
@@ -1,3 +1,7 @@
+import { defineCustomElements } from '../q/dist/esm/es2017/q-components.define';
+
 (function () {
+    defineCustomElements(window);
+
     console.log(`Sample app is running...`);
 })();
